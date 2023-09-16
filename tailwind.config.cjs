dd87127f27e2f5c85c9948419ffaf4c419c58ec9
DavidPav3l <david_marian@outlook.com>
2023-09-16 21:38:45 +0300

    Landing-ul e gata

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				MontSerrat: ['Montserrat Variable', 'MontSerrat', 'sans-serif'],
+				SansPro: ['Source Sans Pro', 'sans-serif']
+			},
+			colors: {
+				cream: '#f5eddf'
+			}
+		}
+	},
+	plugins: []
+}
