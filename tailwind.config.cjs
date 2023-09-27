f5af775a4e3dbc012dac66f8adf528055ab5ea7d
popibeatrice <popicabeatrice31@gmail.com>
2023-09-27 22:11:13 +0300

    safari suge

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			xs: '375px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			'lg-xl': '1050px',
+			xl: '1280px',
+			'2xl': '1536px'
+		},
 		extend: {
 			fontFamily: {
 				MontSerrat: ['Montserrat Variable', 'MontSerrat', 'sans-serif'],
@@ -9,9 +19,6 @@ module.exports = {
 			},
 			colors: {
 				cream: '#f5eddf'
-			},
-			screens: {
-				xs: '375px'
 			}
 		}
 	},
