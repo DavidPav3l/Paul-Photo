84de6e23511d7a249b7827f692f0af330957289c
popibeatrice <popicabeatrice31@gmail.com>
2023-10-06 16:57:24 +0300

    Gata liceul

--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -9,7 +9,7 @@ export default function ReadMore({ children }) {
 	return (
 		<>
 			{children}
-			<div className="animate-showUp relative z-10 m-8 w-[80%] max-w-2xl leading-loose 2xl:text-lg">
+			<div className="relative z-10 m-8 w-[80%] max-w-xl animate-showUp leading-loose xl:max-w-2xl 2xl:text-lg">
 				{!readMore ? (
 					<div>
 						Imaginile cu natura în diversele ei manifestări, precum și cele cu oamenii și locurile
